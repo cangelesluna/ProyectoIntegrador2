2f1011721d50410897b1a5ab5b9970d9678e0fed
cesar alberto angeles luna <cangelesluna@gmail.com>
2026-06-25 18:09:52 -0500

    Fix GitHub Pages router base

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const routerBasename = import.meta.env.BASE_URL === '/'
+  ? '/'
+  : import.meta.env.BASE_URL.replace(/\/$/, '');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={routerBasename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
